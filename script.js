f36ef9dcd75e10048692d9f0a650fc4ee93f19fb
kruzznitsch <rougemeister1@gmail.com>
2024-06-20 12:26:10 +0100

    Added animation styles

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
 const adviceText = document.querySelector("#advice-text");
 const adviceId = document.querySelector("#advice-id");
 const button = document.querySelector("#dice-icon");
+const animationEl = document.querySelector('.animation')
+console.log(animationEl)
 
 const getAdvice = async () => {
   try {
   const res = await axios.get("https://api.adviceslip.com/advice");
   const id = res.data.slip.id;
   const advice = res.data.slip.advice;
-  adviceText.innerHTML = `"${advice}"`;
-  adviceId.innerHTML = `Advice #${id}`;
+  adviceText.innerText = `"${advice}"`;
+  adviceId.innerText = `Advice #${id}`;
 }
 
-  catch (err) {
-    adviceText.textContent = "NO ADVICE AVAILABLE!";
-  }
+  catch (e) {
+    adviceText.textContent = "";
+    animationEl.style.display = 'flex'
+  } 
 };
 
 button.addEventListener("click", getAdvice);
